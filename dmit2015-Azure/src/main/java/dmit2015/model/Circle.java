bfd58effe7d40878f98935f54dd6c2e3efda3c74
WenlingLyu <wlyu1@nait.ca>
2023-01-13 11:54:52 -0700

    created auzre demo project

--- dmit2015-Azure/src/main/java/dmit2015/model/Circle.java
+++ dmit2015-Azure/src/main/java/dmit2015/model/Circle.java
@@ -37,10 +37,35 @@ public class Circle {
         return Math.PI * radius*radius;
     }
     // method
-    public double a(){
+    public double Perimeter(){
         return Math.PI*radius*2;
     }
+    public static void main(String[] args){
+        // create a new instance of circle but did not assign it's value
+    Circle circle1 = new Circle();
 
+    //Assign a value to radius
+        // Circle circle1 = new Circle(10);
+        //OR
+        // Circle circle1 = new Circle();
+        // circle1.radius = 10;
 
-    
+    //circle1.radius = 5;
+        // % -- followning with the number you wants to get
+        // \n start a new line
+    System.out.printf("The radius og circle1 is %s\n",circle1.getRadius());
+    System.out.printf("The area of circle1 is %.2f(2 decimal),\n",circle1.Area());
+    System.out.printf("The perimeter of circle1 is %.2f",circle1.Perimeter());
+
+    try {
+        circle1.setRadius(-25);
+        System.out.printf("A exception should have been thrown");
+    }
+    catch (Exception ex){
+        System.out.printf(ex.getMessage());
+    }
+
+    }
+
 }
+
