be30ff5e95e2f762db0b74fc70542e85a20dbdb1
WenlingLyu <wlyu1@nait.ca>
2023-02-01 15:44:51 -0700

    test

--- dmit2015-Azure/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
+++ dmit2015-Azure/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
@@ -2,48 +2,38 @@ package dmit2015.model;
 
 import lombok.Getter;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CanadianIncomeTaxManager {
-    //define a private constructor to implement single pattern
-    private CanadianIncomeTaxManager(){
+
+    // Define a private constructor to implement Single pattern
+    private CanadianIncomeTaxManager() {
 
     }
-    //define a single instance of this class
-    private static  CanadianIncomeTaxManager INSTANCE;
 
-    //define a static class-level to access the singleton
-    public static CanadianIncomeTaxManager getInstance(){
-        if(INSTANCE == null){
+    // Define a single instance of this class
+    private static CanadianIncomeTaxManager INSTANCE;
+
+    // Define a static class-level to access the singleton
+    public static CanadianIncomeTaxManager getInstance() {
+        if (INSTANCE == null) {
             INSTANCE = new CanadianIncomeTaxManager();
         }
         return INSTANCE;
     }
+
     @Getter
-    public List<CanadianPersonalIncomeTaxRate> incomeTaxRates;
-    public List<String> fetchIncomeTaxData(){
-        List<String> incomeTaxData = new ArrayList<>();
-        Path csvPath = null;
-        try {
-            csvPath = Path.of(
-                    getClass()
-                            .getClassLoader()
-                            .getResource("data/CanadianPersonalIncomeTaxRates")
-                            .toURI());
-            incomeTaxData = Files.readAllLines(csvPath);
-        } catch (URISyntaxException | IOException e) {
-            throw new RuntimeException(e);
-        }
+    private List<CanadianPersonalIncomeTaxRate> incomeTaxRates;
 
-        // Use the following code to access a resource file not stored in your project
-        // Path csvPath = Path.of("d:/temp/electricity-exports-and-imports-data-dictionary.csv");
-
-        // Use the `Files.readAllLines()` method to read all lines from a file as a List.
-        return incomeTaxData;
+    public void loadDataFromFile() {
+//        try {
+//            Path csvPath = Path.of(
+//                    getClass()
+//                            .getClassLoader()
+//                            .getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
+//            //incomeTaxRates = Files.readAllLines(csvPath);
+//        } catch (URISyntaxException | IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
--- dmit2015-Azure/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRate.java
+++ dmit2015-Azure/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxRate.java
@@ -6,41 +6,49 @@ import java.util.Optional;
 
 @Data
 public class CanadianPersonalIncomeTaxRate {
-    private  String RegionAbbreviation;
-    private String RegionName;
+    private String regionAbbreviation;
+
+    private String regionName;
+
     private int taxYear;
+
     private int bracketNo;
 
-    //private BigDecimal startingIncome;
+    //    private BigDecimal startingIncome;
     private double startingIncome;
-    private Double endingIncome;  // Double means this filed can not have value -- nullable
+
+    private Double endingIncome;
+
     private double taxRate;
+
     private double baseTax;
 
-    public static Optional<CanadianPersonalIncomeTaxRate> parseCsv(String line){
+    public static Optional<CanadianPersonalIncomeTaxRate> parseCsv(String line) {
         Optional<CanadianPersonalIncomeTaxRate> optionalCanadianPersonalIncomeTaxRate = Optional.empty();
         final String DELIMITER = ",";
         /*
-        * The folder of columns is
-        * 0)Region Abbreviation
-        * 1)Region Name
-        * 2)Tax Year
-        * 3)BracketNo
-        * 4)Starting Income
-        * 5)Ending Income
-        * 6)Tax Rate
-        * 7)Base Tax Amount
-        * 8)Rounded Base Tax Amount
-        * */
+            The order of columns are:
+            0) Region Abbreviation
+            1) Region Name
+            2) Tax Year
+            3) BracketNo
+            4) Starting Income
+            5) Ending Income
+            6) Tax Rate
+            7) Base Tax Amount
+            8) Rounded Base Tax Amount
+         */
         String[] tokens = line.split(line);
-        if(tokens.length != 9) throw new RuntimeException("Line does not contain exactly 9 values");
+        if (tokens.length != 9) {
+            throw new RuntimeException("Line does not contain exactly 9 values");
+        }
         String regionAbbrev = tokens[0];
         String regionName = tokens[1];
         int taxYear = Integer.parseInt(tokens[2]);
         int bracketNo = Integer.parseInt(tokens[3]);
         double startingIncome = Double.parseDouble(tokens[4]);
-//        double endingIncome = tokens[5].isBlank() ? null : Double.valueOf(tokens[5]);   // when convert a nullable columns
-        double endingIncome = tokens[5].isBlank() ? Integer.MAX_VALUE : Double.valueOf(tokens[5]);
+        Double endingIncome = tokens[5].isBlank() ? null : Double.valueOf(tokens[5]);
+//        Double endingIncome = tokens[5].isBlank() ? Integer.MAX_VALUE : Double.valueOf(tokens[5]);
         double taxRate = Double.parseDouble(tokens[6]);
         double baseTax = Double.parseDouble(tokens[7]);
         CanadianPersonalIncomeTaxRate currentIncomeTaxRate = new CanadianPersonalIncomeTaxRate();
