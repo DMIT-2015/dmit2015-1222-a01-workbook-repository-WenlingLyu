a0fc44bbc3e63a94be5222c38edf17231bb00908
WenlingLyu <wlyu1@nait.ca>
2023-02-01 16:32:06 -0700

    Difference between each scope Request: only get 1 request ViewScope: Can change but limited to one page SessionScope: Can change between different page but limitet to one browser ApplicationScope: Can change without the limitation of different pages and browser

---
+++ dmit2015-faces-demo/src/main/java/dmit2015/faces/RequestScopeCounterController.java
@@ -0,0 +1,31 @@
+package dmit2015.faces;
+
+import org.omnifaces.util.Messages;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named  // allows you to access an object of this class with the name requestScopeCounterController using EL
+@RequestScoped  // create this object for one HTTP request and destroy after the HTTP response has been sent
+public class RequestScopeCounterController {
+
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public String submit() {
+        counter += 1;
+        Messages.addInfo(null,"RequestScoped Counter = {0}", counter);
+        Messages.addInfo("counterResult", "RequestScoped Count Result = {0}", counter);
+        return "";
+    }
+}
+
+
+// Difference between each scope
+// Request: only get 1 request
+// ViewScope: Can change but limited to one page
+// SessionScope: Can change between different page but limited to one browser
+// ApplicationScope: Can change without the limitation of different pages and browser
